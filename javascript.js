b7c28735006bf6b11bf37d537d14a364cfe0b9e3
Rafia Anzum <dlagrandy@gmail.com>
2024-03-19 22:04:06 +0600

    add tiles in pixel art canvas

---
+++ javascript.js
@@ -0,0 +1,17 @@
+let container = document.querySelector(".container");
+let containerWidth = window.getComputedStyle(container).getPropertyValue("width").slice(0, -2);
+let containerHeight = window.getComputedStyle(container).getPropertyValue("height").slice(0, -2);
+
+let totalTiles = 16;
+
+for (let column = 0; column < totalTiles; ++column) {
+    for (let row = 0; row < totalTiles; ++row) {
+        let tile = document.createElement("div");
+        if (row%2 === 0 && column % 2 === 0 || row % 2 !== 0 && column % 2 !== 0) {
+            tile.style.backgroundColor = "#d9d9d9";
+        }
+        tile.style.width = `${containerWidth/totalTiles}px`;
+        tile.style.height = `${containerHeight/totalTiles}px`;
+        container.appendChild(tile);
+    }
+}
