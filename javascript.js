2cec09b420c0cdb738b3893038a84ece23d9472b
Rafia Anzum <dlagrandy@gmail.com>
2024-03-20 16:18:52 +0600

    change color of tiles

--- javascript.js
+++ javascript.js
@@ -8,8 +8,8 @@ for (let column = 0; column < totalTiles; ++column) {
     for (let row = 0; row < totalTiles; ++row) {
         let tile = document.createElement("div");
         if (row%2 === 0 && column % 2 === 0 || row % 2 !== 0 && column % 2 !== 0) {
-            tile.style.backgroundColor = "#d9d9d9";
-        }
+            tile.style.backgroundColor = "#d9d9d9ec";
+        } else tile.style.backgroundColor = "#fffffff5";
         tile.style.width = `${containerWidth/totalTiles}px`;
         tile.style.height = `${containerHeight/totalTiles}px`;
         container.appendChild(tile);
