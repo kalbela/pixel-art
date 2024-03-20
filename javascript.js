00247fc0ef01de6f968ce91eed229a099f44de18
Rafia Anzum <dlagrandy@gmail.com>
2024-03-20 16:56:28 +0600

    change tiles number based on user input

--- javascript.js
+++ javascript.js
@@ -2,16 +2,30 @@ let container = document.querySelector(".canvas-container");
 let containerWidth = window.getComputedStyle(container).getPropertyValue("width").slice(0, -2);
 let containerHeight = window.getComputedStyle(container).getPropertyValue("height").slice(0, -2);
 
-let totalTiles = 16;
+let range = document.querySelector("input[type=range]");
+let rangeMsg = document.querySelector(".range-msg");
 
-for (let column = 0; column < totalTiles; ++column) {
-    for (let row = 0; row < totalTiles; ++row) {
-        let tile = document.createElement("div");
-        if (row%2 === 0 && column % 2 === 0 || row % 2 !== 0 && column % 2 !== 0) {
-            tile.style.backgroundColor = "#d9d9d9ec";
-        } else tile.style.backgroundColor = "#fffffff5";
-        tile.style.width = `${containerWidth/totalTiles}px`;
-        tile.style.height = `${containerHeight/totalTiles}px`;
-        container.appendChild(tile);
+let totalTiles = range.value;
+createCanvas();
+
+range.addEventListener("input", () => {
+    rangeMsg.textContent = `${range.value} X ${range.value}`;
+    totalTiles = range.value;
+})
+
+range.addEventListener("change", createCanvas);
+
+function createCanvas() {
+    container.replaceChildren();
+    for (let column = 0; column < totalTiles; ++column) {
+        for (let row = 0; row < totalTiles; ++row) {
+            let tile = document.createElement("div");
+            if (row%2 === 0 && column % 2 === 0 || row % 2 !== 0 && column % 2 !== 0) {
+                tile.style.backgroundColor = "#d9d9d9ec";
+            } else tile.style.backgroundColor = "#fffffff5";
+            tile.style.width = `${containerWidth/totalTiles}px`;
+            tile.style.height = `${containerHeight/totalTiles}px`;
+            container.appendChild(tile);
+        }
     }
 }
